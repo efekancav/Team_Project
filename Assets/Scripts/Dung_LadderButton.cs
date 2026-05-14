313fb46fb40eefea703063a9e96beb532553b11d
sanslogic <trashdeity@mail.ru>
2026-05-14 14:30:16 +0200

    Dungeon are levels created
    4 levels
    keys
    doors

---
+++ Assets/Scripts/Dung_LadderButton.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+
+public class Dung_LadderButton : MonoBehaviour
+{
+    public Sprite unpressedSprite;
+    public Sprite pressedSprite;
+
+    public Dung_ExtendingLadder targetLadder;
+
+    private int objectsOnButton = 0;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player") || other.CompareTag("MovableBox"))
+        {
+            objectsOnButton++;
+
+            GetComponent<SpriteRenderer>().sprite = pressedSprite;
+
+            if (targetLadder != null)
+            {
+                targetLadder.RollOut();
+            }
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player") || other.CompareTag("MovableBox"))
+        {
+            objectsOnButton--;
+
+            if (objectsOnButton <= 0)
+            {
+                GetComponent<SpriteRenderer>().sprite = unpressedSprite;
+
+                if (targetLadder != null)
+                {
+                    targetLadder.RollBack(); // 🔥 ВОТ ГЛАВНАЯ СТРОКА
+                }
+            }
+        }
+    }
+}
